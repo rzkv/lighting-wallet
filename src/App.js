5d7ca46c135e837cab0dd33f008af4e8d8099a5c
Rustam Zulkharov <rustam.zulkharov@gmail.com>
2023-01-16 14:30:05 -0500

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import axios from "axios"
+
+function App() {
+  const [price, setPrice] = useState(0)
+
+  const getPrice = () => {
+    axios
+      .get("https://api.coinbase.com/v2/prices/BTC-USD/spot")
+      // .then is a promise that will run when the API call is successful
+      .then((res) => {
+        setPrice(res.data.data.amount);
+      })
+      // .catch is a promise that will run when the API call fails
+      .catch((err) => {
+        console.log(err)
+      });
+  };
+
+  useEffect(() => {
+    getPrice();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Bitcoin Price:</h1>
+      <h1>${price}</h1>
+    </div>
+  );
+}
+
+export default App;
+
